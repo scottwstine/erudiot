b875a5fb7056b80291b2404d5b376c46eec4a40f
scottwstine <scottwstine@gmail.com>
2019-02-28 16:29:56 -0800

    working on users

--- booklite/urls.py
+++ booklite/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'booklite'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('save_book/', views.save_book, name='save_book')
+    path('save_book/', views.save_book, name='save_book'),
+    path('register_user/', views.register_user, name='register_user'),
+    path('registration/', views.registration, name='registration')
 ]
--- booklite/views.py
+++ booklite/views.py
@@ -1,17 +1,51 @@
 from django.shortcuts import render, reverse
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from .models import Book
 import json
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
 
 def index(request):  
-    return render(request, 'index.html', {})
+    return render(request, 'booklite/index.html', {})
 
 def save_book(request):
     data = json.loads(request.body)
+    print(data)
     title = data['title']
     author = data['author']
-    published = data['published']
-    publisher = data['publisher']
+    # published = data['published']
+    # publisher = data['publisher']
     img_url = data['cover_url']
-    Book.save()
+    # book = Book(title=title, author=author, ..)
+    # book.save()
+    # print(title)
     return HttpResponse('Book successfully saved')
+
+def registration(request):
+    return render(request, 'booklite/registration.html', {})
+
+def register_user(request):     
+    username = request.POST['username']    
+    password = request.POST['password']
+    # confirm_password = request.POST('confirm_password')
+    # if password != confirm_password:
+    #     return HttpResponseRedirect(reverse('booklite:registration'))
+    user = User.objects.create_user(username, password)
+    login(request, user)
+    
+    return HttpResponseRedirect(reverse('booklite:index'))
+
+
+# def login_user(request):
+#     username = request.POST('username')
+#     password = request.POST('password')
+#     user = authenticate(request, username=username, password=password)
+#     if user is not None:
+#         login(request, user)
+
+# def logout_user(request):
+#     logout(request)
+#     return HttpResponseRedirect(reverse('booklite:index'))
+
