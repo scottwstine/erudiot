6a86cd0adbd784e7c51b5da6b9f9fb6384dd8b93
scottwstine <scottwstine@gmail.com>
2019-03-22 13:10:47 -0700

    updates 3.22

--- booklite/models.py
+++ booklite/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=50)
-    # genre = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)    
     description = models.TextField(max_length=None)
     published = models.CharField(max_length=30)
     publisher = models.CharField(max_length=50)
@@ -12,9 +19,7 @@ class Book(models.Model):
     img_url = models.CharField(max_length=200)
     users = models.ManyToManyField(User, related_name='books')
     isbn = models.CharField(max_length=13)
+    genres = models.ManyToManyField(Genre, related_name='books')
 
     def __str__(self):
         return self.title
-
-
-
--- booklite/views.py
+++ booklite/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import render, reverse
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
-from .models import Book
+from .models import Book, Genre
 import json
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
 
-def index(request):  
+def index(request):
     return render(request, 'booklite/index.html', {})
 
-# @login_required
+
 def save_book(request):
     if not request.user.is_authenticated:
         return HttpResponse('not logged in')
     data = json.loads(request.body)
-    print(data) 
+    print(data)
     title = data['title']
     author = data['author']
     published = data['published']
@@ -27,27 +27,33 @@ def save_book(request):
     if Book.objects.filter(isbn=isbn).exists():
         book = Book.objects.get(isbn=isbn)
     else:
-        book = Book(title=title, author=author, description=description, publisher=publisher, published=published, img_url=img_url, pagecount=pagecount, isbn=isbn)
-        book.save()    
+        book = Book(title=title, author=author, description=description, publisher=publisher,
+                    published=published, img_url=img_url, pagecount=pagecount, isbn=isbn)
+        book.save()
     book.users.add(request.user)
-    book.save()
 
-    # print(title)
+    genre_names = data['genres']
+    for genre_name in genre_names:
+        genre, created = Genre.objects.get_or_create(name=genre_name)
+        book.genres.add(genre)
+    
     return HttpResponse('Book successfully saved')
 
-def registration(request): 
+
+def registration(request):
     next = request.GET.get('next', '')
     return render(request, 'booklite/registration.html', {'next': next})
 
-def register_user(request):     
-    username = request.POST['username']    
+
+def register_user(request):
+    username = request.POST['username']
     password = request.POST['password']
-    confirm_password = request.POST('confirm_password')
+    confirm_password = request.POST['confirm_password']
     if password != confirm_password:
         return HttpResponseRedirect(reverse('booklite:registration'))
     user = User.objects.create_user(username, password)
     login(request, user)
-    
+
     return HttpResponseRedirect(reverse('booklite:index'))
 
 
@@ -66,28 +72,26 @@ def login_user(request):
         return HttpResponseRedirect(next)
     return HttpResponseRedirect(reverse('booklite:registration'))
 
-    
 
 def logout_user(request):
     logout(request)
     return HttpResponseRedirect(reverse('booklite:index'))
 
+
 @login_required
-def my_books(request):        
+def my_books(request):
     books = request.user.books.all()
     return render(request, 'booklite/my_books.html', {'books': books})
 
 
-def remove_book(request, book_id):    
+def remove_book(request, book_id):
     book = request.user.books.get(id=book_id)
     request.user.books.remove(book)
-    
+
     return HttpResponseRedirect(reverse('booklite:my_books'))
-    
-    
+
+
 def book_details(request, book_id):
     book = request.user.books.get(id=book_id)
-    
-    return render(request, 'booklite/book_details.html', {'book': book})
 
- 
+    return render(request, 'booklite/book_details.html', {'book': book})
